cbb7f159d7cec732c162310170d3deea2150bddb
JamShou <jamalshoubakics@gmail.com>
2024-11-11 22:26:40 -0600

    moved movies and genres to app.js hierarchy to support watchlist feature, refactored code and error handling

--- src/App.js
+++ src/App.js
@@ -5,8 +5,55 @@ import Footer from "./components/Footer";
 import MoviesGrid from "./components/MoviesGrid";
 import Watchlist from "./components/Watchlist";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+
+const fetchData = async (url) => {
+  try {
+    const response = await fetch(url);
+    if (!response.ok) throw new Error(`Error: ${response.statusText}`);
+    return await response.json();
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+};
 
 function App() {
+  const [page, setPage] = useState(1); // Track current page
+
+  const API_KEY = process.env.REACT_APP_TMDB_API_KEY;
+  const GENRES_URL = `https://api.themoviedb.org/3/genre/movie/list?api_key=${API_KEY}&language=en-US`;
+
+  if (!API_KEY) console.error("API key is missing!");
+
+  // Dynamic API URL based on the page
+  const API_URL = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=${page}`;
+
+  const [movies, setMovies] = useState([]);
+  const [genres, setGenres] = useState([]);
+
+  useEffect(() => {
+    const fetchAllData = async () => {
+      const genreData = await fetchData(GENRES_URL);
+      const movieData = await fetchData(API_URL);
+      if (genreData) {
+        setGenres(genreData.genres || []);
+      }
+      if (movieData) {
+        setMovies(movieData.results || []);
+      }
+    };
+    fetchAllData();
+  }, [API_URL, GENRES_URL, page]);
+
+  const handleNextPage = () => {
+    setPage((prevPage) => prevPage + 1); // Go to next page
+  };
+
+  const handlePrevPage = () => {
+    if (page > 1) setPage((prevPage) => prevPage - 1); // Prevent going to page 0
+  };
+
   return (
     <div className="App">
       <div className="container">
@@ -22,11 +69,30 @@ function App() {
               </li>
             </ul>
           </nav>
-          <Routes>
-            <Route path="/" element={<MoviesGrid />}></Route>
-            <Route path="/watchlist" element={<Watchlist />}></Route>
-          </Routes>
+          {/* Check for movies and show an error message if empty */}
+          {movies && movies.length === 0 ? (
+            <p className="error-message">
+              Check API Key: Movies could not be loaded
+            </p>
+          ) : (
+            <Routes>
+              <Route
+                path="/"
+                element={<MoviesGrid movies={movies} genres={genres} />}
+              />
+              <Route path="/watchlist" element={<Watchlist />} />
+            </Routes>
+          )}
         </Router>
+      </div>
+
+      {/* Pagination controls */}
+      <div className="pagination">
+        <button onClick={handlePrevPage} disabled={page === 1}>
+          Previous
+        </button>
+        <span>Page {page}</span>
+        <button onClick={handleNextPage}>Next</button>
       </div>
 
       <Footer />
--- src/components/MoviesGrid.js
+++ src/components/MoviesGrid.js
@@ -1,58 +1,18 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import MovieCard from "./MovieCard/MovieCard";
 import "../styles.css";
 
-const fetchData = async (url) => {
-  try {
-    const response = await fetch(url);
-    if (!response.ok) throw new Error(`Error: ${response.statusText}`);
-    return await response.json();
-  } catch (error) {
-    console.error(error);
-    return null;
-  }
-};
-
-export default function MoviesGrid() {
-  const [movies, setMovies] = useState([]);
-  const [genres, setGenres] = useState([]);
+export default function MoviesGrid({ movies, genres }) {
   const [selectedMovie, setSelectedMovie] = useState(null);
-  const [page, setPage] = useState(1); // Track current page
   const [searchTerm, setSearchTerm] = useState("");
 
-  const API_KEY = process.env.REACT_APP_TMDB_API_KEY;
-  const GENRES_URL = `https://api.themoviedb.org/3/genre/movie/list?api_key=${API_KEY}&language=en-US`;
-
-  if (!API_KEY) console.error("API key is missing!");
-
-  // Dynamic API URL based on the page
-  const API_URL = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=${page}`;
-
   const [genre, setGenre] = useState("All Genres");
-  const [rating, setRating] = useState("All");
-
-  useEffect(() => {
-    const fetchAllData = async () => {
-      const genreData = await fetchData(GENRES_URL);
-      const movieData = await fetchData(API_URL);
-      if (genreData) setGenres(genreData.genres || []);
-      if (movieData) setMovies(movieData.results || []);
-    };
-    fetchAllData();
-  }, [API_URL, GENRES_URL, page]); // Dependency on page for API call
+  const [rating, setRating] = useState("All Ratings");
 
   const handleCardClick = (movie) => {
     setSelectedMovie((prevState) =>
       prevState && prevState.id === movie.id ? null : movie
     );
-  };
-
-  const handleNextPage = () => {
-    setPage((prevPage) => prevPage + 1); // Go to next page
-  };
-
-  const handlePrevPage = () => {
-    if (page > 1) setPage((prevPage) => prevPage - 1); // Prevent going to page 0
   };
 
   const handleSearchChange = (e) => {
@@ -81,7 +41,7 @@ export default function MoviesGrid() {
 
   const matchesRating = (movie, rating) => {
     switch (rating) {
-      case "All":
+      case "All Ratings":
         return true;
 
       case "Good":
@@ -98,13 +58,13 @@ export default function MoviesGrid() {
     }
   };
 
-  const matchesSearchTerm = (movie, genre) => {
+  const matchesSearchTerm = (movie) => {
     return movie.title.toLowerCase().includes(searchTerm.toLowerCase());
   };
 
   const filteredMovies = movies.filter(
     (movie) =>
-      matchesSearchTerm(movie, searchTerm) &&
+      matchesSearchTerm(movie) &&
       matchesGenre(movie, genre) &&
       matchesRating(movie, rating)
   );
@@ -119,64 +79,47 @@ export default function MoviesGrid() {
         onChange={handleSearchChange}
       />
       <div>
-        {movies.length === 0 ? (
-          <p className="error-message">
-            Check API Key: Movies could not be loaded
-          </p>
-        ) : (
-          <>
-            <div className="filter-bar">
-              <div className="filter-slot">
-                <label>Genre</label>
-                <select
-                  className="filter-dropdown"
-                  value={genre}
-                  onChange={handleGenreChange}
-                >
-                  <option value="All Genres">All Genres</option>
-                  {genres.map((genre) => (
-                    <option key={genre.id} value={genre.name}>
-                      {genre.name}
-                    </option>
-                  ))}
-                </select>
-              </div>
-              <div className="filter-slot">
-                <label>Rating</label>
-                <select
-                  className="filter-dropdown"
-                  value={rating}
-                  onChange={handleRatingChange}
-                >
-                  <option>All Rating</option>
-                  <option>Good</option>
-                  <option>Ok</option>
-                  <option>Bad</option>
-                </select>
-              </div>
-            </div>
-            <div className="movies-grid">
-              {filteredMovies.map((movie) => (
-                <MovieCard
-                  key={movie.id}
-                  movie={movie}
-                  genres={genres}
-                  isSelected={selectedMovie && selectedMovie.id === movie.id}
-                  onClick={handleCardClick}
-                />
+        <div className="filter-bar">
+          <div className="filter-slot">
+            <label>Genre</label>
+            <select
+              className="filter-dropdown"
+              value={genre}
+              onChange={handleGenreChange}
+            >
+              <option value="All Genres">All Genres</option>
+              {genres.map((genre) => (
+                <option key={genre.id} value={genre.name}>
+                  {genre.name}
+                </option>
               ))}
-            </div>
-
-            {/* Pagination controls */}
-            <div className="pagination">
-              <button onClick={handlePrevPage} disabled={page === 1}>
-                Previous
-              </button>
-              <span>Page {page}</span>
-              <button onClick={handleNextPage}>Next</button>
-            </div>
-          </>
-        )}
+            </select>
+          </div>
+          <div className="filter-slot">
+            <label>Rating</label>
+            <select
+              className="filter-dropdown"
+              value={rating}
+              onChange={handleRatingChange}
+            >
+              <option>All Ratings</option>
+              <option>Good</option>
+              <option>Ok</option>
+              <option>Bad</option>
+            </select>
+          </div>
+        </div>
+        <div className="movies-grid">
+          {filteredMovies.map((movie) => (
+            <MovieCard
+              key={movie.id}
+              movie={movie}
+              genres={genres}
+              isSelected={selectedMovie && selectedMovie.id === movie.id}
+              onClick={handleCardClick}
+            />
+          ))}
+        </div>
       </div>
     </div>
   );
